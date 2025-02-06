e149be1e69957f2334c69c5aeeb73fdfc1cebc3e
d.wong92 <d.wong92@gmail.com>
2025-02-06 12:30:44 -0500

    added back button

--- client/src/containers/TrainStatusPage.jsx
+++ client/src/containers/TrainStatusPage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SubwayButton from "../components/SubwayButton";
+import backButton from "../assets/backButton.png";
 
 export default function TrainStatusPage({ trainGroups }) {
   return (
     <div>
       <div className="trainButtonsContainer">
+        <Link to={"/"}>
+          <div className="trainButtonsContainer">
+            <button className="mtaLineButton">
+              <img className="train-icon" src={backButton} alt="BackButton" />
+            </button>
+          </div>
+        </Link>
         <SubwayButton trainGroups={trainGroups} />
       </div>
     </div>
